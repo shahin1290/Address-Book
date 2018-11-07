01771a2675ef421a6a02c93159cca1a10073f8b6
shahin1290 <s.patowary@yahoo.com>
2018-11-07 10:25:35 +0100

    added new features and updated basic-steps.js

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,11 +1,31 @@
-const { Given, Then } = require("cucumber");
+const { Given, Then, When } = require('cucumber')
 
-Given("I visit the site", function() {
+Given('I visit the site', async function() {
   // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
+  return 'pending'
+})
 
-Then("I should see {string}", function(string) {
+Then('I should see {string}', async function(content) {
   // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
+  return 'pending'
+})
+
+When('I click {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I fill in {string} with {string}', async function(string, string2) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should have {int} contact in my address book', async function(int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should not see {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
