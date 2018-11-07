bb0549d24525be4ebee1325753d9b797f67de9a5
shahin1290 <s.patowary@yahoo.com>
2018-11-07 13:49:30 +0100

    added render contacts on the page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,55 @@
+
+const renderContacts = () => {
+  const storage = window.localStorage
+  // Read all the contacts from the storage
+  const contacts = JSON.parse(storage.getItem('contacts'))
+
+  // Select the container we will use to list the contacts 
+  let div = document.querySelector('.contact-list')
+
+  if (contacts) {
+    div.innerHTML = ''
+
+    // render the contacts
+    const ul = document.createElement('ul')
+
+    // For every contact in our array of contacts, we will
+    // create a li element that will contain a card with
+    // all the information of the contact
+    contacts.forEach(contact => {
+      let li = document.createElement('li')
+      li.innerHTML = `
+        <div class="card">
+          <div class="image">
+            <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
+          </div>
+          <div class="content">
+            <h1>${ contact.name }</h1>
+            <h2>${ contact.company }</h2>
+            <p>${ contact.notes }</p> 
+            ${ contact.email } | 
+            <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+          </div>
+        </div>
+     `
+      // Add the contact's li to the unordered list we created earlier
+      ul.appendChild(li)
+    })
+
+    // Lastly, append the list to the contact-list container.
+    div.appendChild(ul) 
+  } else { 
+    div.innerHTML = '<p>You have no contacts in your address book</p>' 
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    renderContacts()
+
     // Select form object from the DOM
     const addContactForm = document.querySelector('.new-contact-form')
+
+    
   
     // Register an event to listen for form submission
     addContactForm.addEventListener('submit', event => {
@@ -8,8 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
       event.preventDefault()
 
       const storage = window.localStorage
-
-  
+ 
       // Get all inputs elements from the form
       const {
         name,
@@ -31,7 +79,10 @@ document.addEventListener('DOMContentLoaded', () => {
         twitter: twitter.value,
       }
   
-      console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
-      storage.setItem('contacts', JSON.stringify([contact]))
+    console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
+    storage.setItem('contacts', JSON.stringify([contact]))
+    renderContacts()
+
     })
+   
   })
