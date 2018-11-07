b6236c967b6d4703524ca29f29a545c45fe6b0b9
shahin1290 <s.patowary@yahoo.com>
2018-11-07 10:36:58 +0100

    created Adressbook class, added open and close homepage functionality

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,8 +1,11 @@
-const { Given, Then, When } = require('cucumber')
+const { After, Given, Then, When } = require('cucumber')
+
+After(async function() {
+  return await this.closeHomePage()
+})
 
 Given('I visit the site', async function() {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  return await this.openHomePage()
 })
 
 Then('I should see {string}', async function(content) {
